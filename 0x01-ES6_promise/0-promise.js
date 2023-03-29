24dbb9cd3f526874ef4d703dbee27a325667a584
TabbieManywanda <achiengtabitha@gmail.com>
2023-03-29 13:01:27 +0000

    Edit

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,19 +1,5 @@
 function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    const x = 1 + 1;
-    if (x === 2) {
-      resolve('Success');
-    } else {
-      reject(new Error('Error'));
-    }
-  });
+  return Promise.resolve();
 }
 
-const promise = getResponseFromAPI();
-promise
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((error) => {
-    console.log(error.name);
-  });
+export default getResponseFromAPI;
