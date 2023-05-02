eb9198e1d0e1265b4b4d6ec81a3436eb0e66567a
TabbieManywanda <achiengtabitha@gmail.com>
2023-05-02 11:18:13 +0000

    Edit

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,14 +1,8 @@
 import ClassRoom from './0-classroom';
 
-function initializeRooms() {
-  const sizes = [19, 20, 34];
-  const obj = [];
-
-  for (let i = 0; i < sizes.length; i + 1) {
-    const classSize = new ClassRoom(sizes[i]);
-    obj.push(classSize);
-  }
-  return obj;
-}
+const initializeRooms = () => {
+  const array = [19, 20, 34];
+  return array.map((n) => new ClassRoom(n));
+};
 
 export default initializeRooms;
