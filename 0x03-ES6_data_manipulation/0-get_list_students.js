794b6c878102550b271c710934719121e0951864
TabbieManywanda <achiengtabitha@gmail.com>
2023-06-26 11:27:36 +0000

    Edit

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,19 +1,29 @@
-const getListStudents = () => [
-  {
-    id: 1,
-    firstName: 'Guillaume',
-    location: 'San Francisco',
-  },
-  {
-    id: 2,
-    firstName: 'James',
-    location: 'Columbia',
-  },
-  {
-    id: 5,
-    firstName: 'Serena',
-    location: 'San Francisco',
-  },
-];
+class Students {
+  constructor(id, name, country) {
+    this.id = id;
+    this.name = name;
+    this.country = country;
+  }
 
-export default getListStudents;
+  get() {
+    const obj = {
+      id: this.id,
+      firstName: this.name,
+      location: this.country,
+    };
+    return (obj);
+  }
+}
+
+export default function getListStudents() {
+  const array = ['Guillaume', 1, 'San Francisco'];
+  const array1 = ['James', 2, 'Columbia'];
+  const array2 = ['Serena', 5, 'San Francisco'];
+
+  const full = [array, array1, array2];
+  const returnArray = [];
+  for (const x of full) {
+    returnArray.push(new Students(x[1], x[0], x[2]).get());
+  }
+  return (returnArray);
+}
